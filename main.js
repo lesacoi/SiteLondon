96f85789a32332c897b4a45fcc25419bd4325003
lesacoi <leosailhac@gmail.com>
2025-05-09 00:04:37 +0200

    Add ciel

--- main.js
+++ main.js
@@ -6,3 +6,21 @@ function hideDescription(id) {
   const descriptionDiv = document.getElementById(id);
   descriptionDiv.style.display = "none";
 }
+
+const audio = document.getElementById("audioPlayer");
+const playPauseBtn = document.getElementById("playPauseBtn");
+
+playPauseBtn.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.play();
+    playPauseBtn.textContent = "⏸️";
+  } else {
+    audio.pause();
+    playPauseBtn.textContent = "▶️";
+  }
+});
+
+// Remettre le bouton en "play" quand l'audio est fini
+audio.addEventListener("ended", () => {
+  playPauseBtn.textContent = "▶️";
+});
